cad48fbd46da9a28a4cb69ca6ff12e4e4391dde7
pbalzac <pbalzac@tutanota.com>
2020-11-23 23:10:01 -0700

    24.2 finish

--- 24/24.rb
+++ 24/24.rb
@@ -74,16 +74,155 @@ def smallest(nums)
   result
 end
 
+def recurse(nums, weight, size, start, remaining)
+  groups = []
+  found = false
+  min_length_groups = []
+  if size == start
+    min_length_groups = choose(nums, size).filter { |g| check(g, weight) }
+    start += 1
+  end
+  if min_length_groups.any?
+    puts "Found min_length_groups.length #{min_length_groups.length} remaining #{remaining} nums.length #{nums.length}"
+    if remaining == 1
+      min_length_groups.each do |g|
+        r = [g]
+        if size * 2 == nums.length
+          r += (nums - g)
+        end
+        groups << r
+        found = true
+      end
+    else
+      min_length_groups.each do |g|
+        sub_result = recurse(nums - g, weight, size, size, remaining - 1)
+        if sub_result[:found]
+          sub_result[:groups].each do |sg|
+            r = [g]
+            groups << (r + sg)
+          end
+          found = true
+        end
+      end
+    end
+    p groups
+  else
+    steps = (nums.length - (remaining + 1) * start) / (remaining + 1)
+    puts "steps #{steps}"
+    if remaining == 1
+      (0..steps).each do |step|
+        to_check = choose(nums, start + step).find { |g| check(g, weight) }
+        if !to_check.nil?
+          found = true
+          break
+        end
+      end
+    else
+      (0..steps).each do |step|
+        break if found
+        step_checks = choose(nums, start + step).filter { |g| check(g, weight) }
+        if step_checks.any?
+          step_checks.each do |step_check|
+            sub_result = recurse(nums - step_check, weight,
+                                 size, start + step, remaining - 1)
+            if sub_result[:found]
+              found = true
+              break
+            end
+          end
+        end
+      end
+    end
+  end
+
+  {
+    found: found,
+    groups: groups
+  }
+end
+
+def recurse_bool(nums, weight, start, remaining)
+  found = false
+  steps = (nums.length - (remaining + 1) * start) / (remaining + 1)
+    if remaining == 1
+      (0..steps).each do |step|
+        to_check = choose(nums, start + step).find { |g| check(g, weight) }
+        if !to_check.nil?
+          found = true
+          break
+        end
+      end
+    else
+      (0..steps).each do |step|
+        break if found
+        step_checks = choose(nums, start + step).filter { |g| check(g, weight) }
+        if step_checks.any?
+          step_checks.each do |step_check|
+            found = recurse_bool(nums - step_check, weight, start + step, remaining - 1)
+            break if found
+          end
+        end
+      end
+    end
+    found
+end
+
+def general_bool(nums, x)
+  results = []
+  total = nums.reduce(&:+)
+  weight = total / x
+  puts "TOTAL WEIGHT #{total} PER GROUP #{weight}"
+  (1..nums.length / x).each do |n|
+    break if results.any?
+    puts "Checking #{n}"
+
+    group_ones = choose(nums, n).filter { |g| check(g, weight) }
+    group_ones.each do |g|
+      if recurse_bool(nums - g, weight, n, x - 2)
+        results << g
+      end
+    end
+  end  
+  results
+end
+
+def results_2(nums, x)
+  r = general_bool(nums, x)
+  m = r.min_by { |a| quantum_entanglement(a) }
+  quantum_entanglement(m)
+end
+
+def general(nums, x)
+  result = []
+  total = nums.reduce(&:+)
+  weight = total / x
+  puts "TOTAL WEIGHT #{total} PER GROUP #{weight}"
+
+  (1..nums.length / x).each do |n|
+    return result if result.any?
+    puts "Checking #{n}"
+    sub_result = recurse(nums, weight, n, n, x - 1)
+    if sub_result[:found]
+      result = sub_result[:groups]
+    end
+  end
+  
+  result
+end
+
+def results(groups)
+  minimums = groups.map { |s| s.min_by { |a| quantum_entanglement(a) } }
+  group_one = minimums.min_by { |a| quantum_entanglement(a) }
+  p group_one
+  quantum_entanglement(group_one)
+end
+
 def run(f)
   nums = []
   File.readlines(f).each do |line|
     line.strip!
     nums << line.to_i
   end
-
-  minimums = smallest(nums).map { |s| s.min_by { |a| quantum_entanglement(a) } }
-  group_one = minimums.min_by { |a| quantum_entanglement(a) }
-  p group_one
-  quantum_entanglement(group_one)
+  nums
 end
     
--- 24/README.md
+++ 24/README.md
@@ -1,1 +1,19 @@
 * few bad attempts at this, trying to memoize sums (in case groups were repeated, which they probably wouldn't be a lot?), and only adding when necessary (kept this in), before realizing that only needed to check all groups of the same size, otherwise i could stop as soon as a found that at least one solution was found for a given smallest group...actually i could probably change that filter call to a find for more efficiency
+
+* maybe how it was written, if so really cleverly directed, but because of how the problem description said that if multiple groups of the same size had equal weight, choose the one with the lowest quantum entanglement...i got into all sorts of contortions trying to keep track of when there were multiple groups of the size i was checking. but looking at the description for part two and seeing some of my results from the test made me realize that groupings would be checked twice (e.g. [[A, B], [C, D]] and [[C, D], [A, B]]) so every group of the size to be checked would be first in some iteration and so I could only track the first.
+
+* i like the equation I came up for how deeply I had to check, realizing that going further would just mean checking things that had already been checked, and that I always start at the same size when going deeper in the tree. 
+
+steps = total - (depth + 1) * size / (depth + 1)
+
+2     2       2        24
+3     3	      3	       21	...1
+4     4	      4	       18	...2
+5     5	      5	       15	...3
+6     6	      6	       12	...4
+7     7	      7	       9	...5
+
+depth = 3, total = 30, size = 2
+steps = 30 - (4 * 2) / 4 = 5
+
+* need to clean up code and come up with better names
